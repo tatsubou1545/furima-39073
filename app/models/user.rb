46ae9036550e917f54f50d2a58a7229cd71ddf91
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-03-31 19:02:45 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  validates :nickname, presence: true
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday , presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
 end
