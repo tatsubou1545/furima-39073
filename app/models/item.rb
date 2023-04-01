7f6fb39eea79ca21cb930c687215cb42ad87ae6d
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-02 00:25:29 +0900

    Activehash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
+
   belongs_to :user
   has_one_attached :image
+
+  validates :item_name, :string, presence: true
+  validates :explanation, :text, presence: true
+  validates :price, :integer, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+
 end
