c8864d966f6ac6950d503cb9da44904d815be23c
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-03 00:22:05 +0900

    単体テスト作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :item_name, :explanation, :price, presence: true
+  validates :item_name, :explanation, :image, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
