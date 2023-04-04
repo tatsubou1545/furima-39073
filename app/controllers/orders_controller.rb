cbdbf9dc6860dc09dc835912b01c4b456b937d0d
tatsubou1545 <127082610+tatsubou1545@users.noreply.github.com>
2023-04-04 21:29:02 +0900

    フォームの保存処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,6 @@
 class OrdersController < ApplicationController
 
   def index
-  end
-
-  def new
     @order_address = OrderAddress.new
   end
 
@@ -13,13 +10,13 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :shipping_area_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: Order.item_id)
+    params.require(:order_address).permit(:post_code, :shipping_area_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
